e208788762268c3ee49188771f6d0a9f7e9e1951
Irina <irenserge@mail.ru>
2026-04-08 13:26:55 +0300

    feat: add decision fo number < 100

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
-module.exports = function toReadable(/* number */) {
-  throw new Error('Not implemented');
+module.exports = function toReadable(number) {
+  let value = '';
+  const nums = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+  ];
+  if (number < 10) value = nums[number];
+
+  const teens = [
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'fifteen',
+    'sixteen',
+    'seventeen',
+    'eighteen',
+    'nineteen',
+  ];
+  if (number >= 10 && number < 20) value = teens[number - 10];
+
+  const ties = [
+    'twenty',
+    'thirty',
+    'fourty',
+    'fifty',
+    'sixty',
+    'seventy',
+    'eighty',
+    'ninety',
+  ];
+  if (number >= 20 && number < 100) {
+    value =
+      number % 10 === 0
+        ? ties[number / 10 - 2]
+        : `${ties[Math.floor(number / 10) - 2]} ${ties[number % 10]}`;
+  }
+  return value;
 };
